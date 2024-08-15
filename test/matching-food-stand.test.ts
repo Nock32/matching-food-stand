0757bc87ce1bd858b07b32649ccba6f12b06ca35
Abeshi32 <abe.daisuke@classmethod.jp>
2024-08-15 18:25:51 +0900

    first commit

---
+++ test/matching-food-stand.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MatchingFoodStand from '../lib/matching-food-stand-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/matching-food-stand-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MatchingFoodStand.MatchingFoodStandStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
